451b1916a9069fbfa975ca47612055bd4e294382
varthe <plv9@proton.me>
2024-10-31 21:50:13 +0000

    improved logs

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ RUN npm install
 COPY . .
 EXPOSE 3189
 VOLUME ["/config", "/logs"]
-CMD ["node", "main.js", "/logs/defaulterr.log", "/config/config.yaml", "/config/last_run_timestamps.json"]
+CMD ["node", "main.js", "/logs", "/config/config.yaml", "/config/last_run_timestamps.json"]
--- logger.js
+++ logger.js
@@ -1,17 +1,22 @@
+const path = require("path")
 const winston = require("winston")
+const DailyRotateFile = require("winston-daily-rotate-file")
 
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
-    winston.format.timestamp(),
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
     winston.format.printf(({ timestamp, level, message }) => {
-      return `${timestamp} [${level}]: ${message}`
+      return `${timestamp} [${level.toUpperCase()}]: ${message}`
     })
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({
-      filename: process.argv[2] || "./logs/defaulterr.log",
+    new DailyRotateFile({
+      filename: path.join(process.argv[2] || "./logs", "defaulterr-%DATE%.log"),
+      datePattern: "YYYY-MM-DD",
+      maxSize: "500k",
+      maxFiles: "7d",
     }),
   ],
 })
